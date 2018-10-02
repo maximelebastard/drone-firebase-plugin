2ab567a980c3e5e0b43e2302a0374bc6ffed4ed1
Maxime Lebastard <maxime@pathmotion.com>
2018-10-02 15:49:55 +0200

    Clean unused stuff

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,8 +3,6 @@
 [ -z "$PLUGIN_PROJECT_ID" ] && echo "Please set the \"project_id\" parameter" && exit 1;
 [ -z "$PLUGIN_MESSAGE" ] && echo "Please set the \"message\" parameter" && exit 1;
 [ -z "$PLUGIN_DIR" ] && echo "Please set the \"dir\" parameter" && exit 1;
-[ -z "$PLUGIN_FIREBASE_JSON" ] && echo "Please set the \"firebase_json\" parameter" && exit 1;
-[ -z "$PLUGIN_FIREBASE_RC" ] && echo "Please set the \"firebase_rc\" parameter" && exit 1;
 
 set -o xtrace && \
 
@@ -12,12 +10,6 @@ if [ "$PLUGIN_DEBUG" = true ] ; then
   echo $(pwd) && ls -lah;
 fi && \
 
-
-cp $PLUGIN_FIREBASE_JSON "$PLUGIN_DIR/firebase.json" && \
-cp $PLUGIN_FIREBASE_RC "$PLUGIN_DIR/.firebaserc" && \
-
 cd $PLUGIN_DIR && \
 
-firebase use $PLUGIN_PROJECT_ID && \
-
-firebase deploy --token "$FIREBASE_TOKEN" -m "$PLUGIN_MESSAGE"
+firebase deploy --token "$FIREBASE_TOKEN" -m "$PLUGIN_MESSAGE" --project "$PLUGIN_PROJECT_ID"
